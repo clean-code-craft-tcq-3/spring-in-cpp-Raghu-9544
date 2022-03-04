269056049ea83fe69f8b6dd6068d869b1ac1ed7f
Raghu-9544 <100403037+Raghu-9544@users.noreply.github.com>
2022-03-04 21:33:23 +0530

    Update stats-test.cpp

--- stats-test.cpp
+++ stats-test.cpp
@@ -25,7 +25,7 @@ TEST_CASE("reports average, minimum and maximum") {
     REQUIRE(Catch::isnan(computedStats.min));
 }
 
-/*TEST_CASE("raises alerts when max is greater than threshold") {
+TEST_CASE("raises alerts when max is greater than threshold") {
     EmailAlert emailAlert;
     LEDAlert ledAlert;
     std::vector<IAlerter*> alerters = {&emailAlert, &ledAlert};
@@ -36,4 +36,4 @@ TEST_CASE("reports average, minimum and maximum") {
 
     REQUIRE(emailAlert.emailSent);
     REQUIRE(ledAlert.ledGlows);
-} */
+} 

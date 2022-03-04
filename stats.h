82cc81abc8c070a17b4e131c61d926b7957bbffc
Raghu-9544 <100403037+Raghu-9544@users.noreply.github.com>
2022-03-04 19:00:19 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -2,16 +2,37 @@
 #include <string>
 using namespace std;
 
+
+
 namespace Statistics {
-    Stats ComputeStatistics(const std::vector<double>& data());
+    
+class Stats{
+
+    double Average;
+    double Max;
+    double Min;
+
+    public: 
+    //Constructor
+    Stats(double average, double max, double min)
+    {
+        Average = average;
+        Max = max;
+        Min = min;       
+    }
+
+    };
+    Stats ComputeStatistics(const std::vector<double> data);
+    
 }
 
-struct EmailAlert
-{
-    string emailSent;
-};
 
-struct LEDAlert
-{
+ struct EmailAlert
+ {
+    string emailSent;
+ };
+
+ struct LEDAlert
+ {
     string ledGlows;
 };

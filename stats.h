4ca9549cc08bf31c667d981d7825cf63f3ac46e8
Raghu-9544 <100403037+Raghu-9544@users.noreply.github.com>
2022-03-04 14:15:46 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -1,5 +1,17 @@
 #include <vector>
+#include <string>
+using namespace std;
 
 namespace Statistics {
-    Stats ComputeStatistics(const std::vector<___>& );
+    Stats ComputeStatistics(const std::vector<double>& data());
 }
+
+struct EmailAlert
+{
+    string emailSent;
+};
+
+struct LEDAlert
+{
+    string ledGlows;
+};

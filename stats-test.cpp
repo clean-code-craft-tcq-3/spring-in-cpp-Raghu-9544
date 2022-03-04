ecad0ca5e4cb440d2a6973b7add21a01a8f47f90
Raghu-9544 <100403037+Raghu-9544@users.noreply.github.com>
2022-03-04 21:07:33 +0530

    Update stats-test.cpp

--- stats-test.cpp
+++ stats-test.cpp
@@ -20,9 +20,9 @@ TEST_CASE("reports average, minimum and maximum") {
     
     //Design the REQUIRE statement here.
     //Use http://www.cplusplus.com/reference/cmath/isnan/
-    REQUIRE(isnan(computedStats.average));
-    REQUIRE(isnan(computedStats.max));
-    REQUIRE(isnan(computedStats.min));
+    REQUIRE(Catch::isnan(computedStats.average));
+    REQUIRE(Catch::isnan(computedStats.max));
+    REQUIRE(Catch::isnan(computedStats.min));
 }
 
 /*TEST_CASE("raises alerts when max is greater than threshold") {
